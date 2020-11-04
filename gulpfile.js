8f93164e9610669592bdef9b78bde8c65a220201
Alex Smirnov <alex-wd@yandex.ru>
2020-11-04 19:20:13 +0300

    Gulp & gitignore update

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,54 @@
-// File types to watch
-const fileswatch = 'html,htm,txt,json,md,woff2';
-const srcDirectory = './src';
+var gulp = require('gulp');
+var srcDirectory = './src';
 
 // Modules
-const {src, dest, parallel, series, watch} = require('gulp');
-const scss = require('gulp-sass');
-const cleancss = require('gulp-clean-css');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const autoprefixer = require('gulp-autoprefixer');
+var src = gulp.src;
+var dest = gulp.dest;
+var parallel = gulp.parallel;
+var series = gulp.series;
+var watch = gulp.watch;
+var sass = require('gulp-sass');
+var cleancss = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
 
 // Browser sync
 function browsersync() {
-    browserSync.init({
-        server: {
-            baseDir: 'build/'
-        },
-        notify: false,
-        online: true
-    })
+  browserSync.init({
+    server: {
+      baseDir: 'build/',
+    },
+    notify: false,
+    online: true,
+  });
 }
 
 // Watchers
 function startwatch() {
-    watch(srcDirectory + '/styles' + '/**/*', {usePolling: true}, styles);
-    watch(srcDirectory + '/**/*.html', {usePolling: true}, html);
+  watch(srcDirectory + '/styles' + '/**/*', { usePolling: true }, styles);
+  watch(srcDirectory + '/**/*.html', { usePolling: true }, html);
 }
 
 // Html
 function html() {
-    return src('./src/index.html')
-        .pipe(concat('index.html'))
-        .pipe(dest('./build'))
-        .pipe(browserSync.stream())
+  return src('./src/index.html')
+    .pipe(concat('index.html'))
+    .pipe(dest('./build'))
+    .pipe(browserSync.stream());
 }
 
 // Styles
 function styles() {
-    return src('./src/styles/**/*.scss')
-        .pipe(eval('scss')())
-        .pipe(concat('style.css'))
-        .pipe(autoprefixer({overrideBrowserslist: ['last 10 versions'], grid: true}))
-        .pipe(cleancss({level: {1: {specialComments: 0}},}))
-        .pipe(dest('./build/'))
-        .pipe(browserSync.stream())
+  return src('./src/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(
+      autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })
+    )
+    .pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
+    .pipe(dest('./build/'))
+    .pipe(browserSync.stream());
 }
 
 exports.browsersync = browsersync;
